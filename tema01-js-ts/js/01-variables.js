492ea9fc50e8378e130a45152843a1d75043a66e
Alejandro <alejandrocheroizaguirre1@gmail.com>
2026-09-20 17:52:38 -0500

    update

--- tema01-js-ts/js/01-variables.js
+++ tema01-js-ts/js/01-variables.js
@@ -1,5 +1,18 @@
 function calcularTotal(productos){
     let total = 0;
+
+    for (let i = 0; i < productos.length; i++) {
+        total += productos[i].precio;
     
+    }
+    return total;
 
 }
+
+const productos = [
+    { nombre: "Producto 1", precio: 10 },
+    { nombre: "Producto 2", precio: 20 },
+    { nombre: "Producto 3", precio: 30 }
+];
+
+console.log("Total de productos:", calcularTotal(productos));
